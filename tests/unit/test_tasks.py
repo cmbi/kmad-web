8d08e1d478d5de59cb0cea0fd8e8e1858dfed91f
Joanna Lange <joasialange@gmail.com>
2014-10-28 18:32:08 +0100

    Added tests for get_status and get_result from api.endpoints

--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -3,7 +3,7 @@ import subprocess
 from mock import mock_open, patch
 from nose.tools import eq_, raises
 
-from testdata.test_variables import *
+from testdata import test_variables as test
 from kman_web.factory import create_app, create_celery_app
 
 
@@ -17,34 +17,36 @@ class TestTasks(object):
         cls.celery = create_celery_app(flask_app)
 
     def test_get_seq(self):
-        output = seq
-    
+        output = test.seq
+
         from kman_web.tasks import get_seq
-    
-        result = get_seq.delay('d2p2','>test\nSEQ\n')
+
+        result = get_seq.delay('d2p2', '>test\nSEQ\n')
         eq_(result.get(), output)
-        
 
     @patch('kman_web.tasks.convert_to_7chars')
     @patch('kman_web.tasks.get_fasta_from_blast')
     @patch('subprocess.call')
-    @patch('kman_web.tasks.open', mock_open(read_data=alignment_7c), create=True)
+    @patch('kman_web.tasks.open',
+           mock_open(read_data=test.alignment_7c),
+           create=True)
     def test_align(self, mock_subprocess, mock_call1, mock_call2):
         filename = 'testdata/test.fasta'
         mock_call1.return_value = 'testdata/test.blastp'
         mock_call2.return_value = 'testdata/test.7c'
-        expected = [alignment_1c, alignment_1c_list]
+        expected = [test.alignment_1c, test.alignment_1c_list]
 
         from kman_web.tasks import align
 
         result = align.delay('d2p2', filename)
         eq_(result.get(), expected)
 
-
     @patch('kman_web.tasks.convert_to_7chars')
     @patch('kman_web.tasks.get_fasta_from_blast')
     @patch('subprocess.call')
-    @patch('kman_web.tasks.open', mock_open(read_data=alignment_7c), create=True)
+    @patch('kman_web.tasks.open',
+           mock_open(read_data=test.alignment_7c),
+           create=True)
     @raises(RuntimeError)
     def test_align_subprc_error(self, mock_subprocess, mock_call1, mock_call2):
         mock_call1.return_value = 'testdata/test.blastp'
@@ -56,60 +58,61 @@ class TestTasks(object):
         result = align.delay('d2p2', filename)
         result.get()
 
-
     def test_postprocess(self):
         filename = 'testdata/test.fasta'
-    
+
         from kman_web.tasks import postprocess
-        
-        ## check d2p2 result and output type 'predict'
-        func_input = [seq, d2p2_result]
+
+        # check d2p2 result and output type 'predict'
+        func_input = [test.seq, test.d2p2_result]
         expected = func_input[:]
 
         result = postprocess.delay(func_input, filename, 'predict')
         eq_(result.get(), expected)
 
-        ## check predictors' result and output type 'predict'
-        func_input = [seq] + pred_result 
-        expected = [seq] + processed_pred_result
+        # check predictors' result and output type 'predict'
+        func_input = [test.seq] + test.pred_result
+        expected = [test.seq] + test.processed_pred_result
 
         result = postprocess.delay(func_input, filename, 'predict')
-        a = result.get()
         eq_(result.get(), expected)
-        
+
         # check d2p2 results and output type 'predict_and_align'
-        func_input = [seq] + [d2p2_result] + [[alignment_1c, alignment_1c_list]]
+        func_input = [test.seq] \
+            + [test.d2p2_result] \
+            + [[test.alignment_1c, test.alignment_1c_list]]
         expected = func_input[:]
 
         result = postprocess.delay(func_input, filename, 'predict_and_align')
         eq_(result.get(), expected)
 
         # check predictors' results and output type 'predict_and_align'
-        func_input = [seq] + pred_result + [[alignment_1c, alignment_1c_list]]
-        expected = [seq] + processed_pred_result + [[alignment_1c, alignment_1c_list]]
-        
+        func_input = [test.seq] \
+            + test.pred_result \
+            + [[test.alignment_1c, test.alignment_1c_list]]
+        expected = [test.seq] \
+            + test.processed_pred_result \
+            + [[test.alignment_1c, test.alignment_1c_list]]
+
         result = postprocess.delay(func_input, filename, 'predict_and_align')
         eq_(result.get(), expected)
 
-
     def test_get_task(self):
         output_type = 'predict'
-        
+
         from kman_web.tasks import get_task, postprocess
 
         expected = postprocess
         result = get_task(output_type)
         eq_(result, expected)
 
-
     @raises(ValueError)
     def test_get_value_error(self):
         output_type = 'incorrect_value'
-  
-        from kman_web.tasks import get_task
-  
-        get_task(output_type)
 
+        from kman_web.tasks import get_task
+
+        get_task(output_type)
 
     @patch('kman_web.tasks.find_seqid_blast')
     @patch('subprocess.call')
@@ -119,26 +122,25 @@ class TestTasks(object):
 
         from kman_web.tasks import query_d2p2
 
-        ## check: sequence not found in swissprot
+        # check: sequence not found in swissprot
         mock_call.return_value = [False, '']
         expected = [False, []]
 
         result = query_d2p2.delay(filename)
         eq_(result.get(), expected)
 
-        ## check: sequence found in swissprot, but not found in d2p2
+        # check: sequence found in swissprot, but not found in d2p2
         mock_call.return_value = [True, 'P01542']
         expected = [False, []]
 
         result = query_d2p2.delay(filename)
         eq_(result.get(), expected)
 
-        ## check: sequence found in d2p2
+        # check: sequence found in d2p2
         mock_call.return_value = [True, 'P10636']
 
         result = query_d2p2.delay(filename).get()
         eq_(result[0], True)
-
 
     @patch('subprocess.call')
     @raises(RuntimeError)
@@ -146,63 +148,51 @@ class TestTasks(object):
         filename = 'testdata/test.fasta'
         mock_subprocess.side_effect = subprocess.CalledProcessError(
             "returncode", "cmd", "output")
-        
+
         from kman_web.tasks import query_d2p2
 
         result = query_d2p2.delay(filename)
         result.get()
-        
 
-    @patch('kman_web.tasks.preprocess')    
+    @patch('kman_web.tasks.preprocess')
     @patch('subprocess.call')
-    @patch('kman_web.tasks.open', mock_open(read_data='prediction_out'), create=True)
+    @patch('kman_web.tasks.open',
+           mock_open(read_data='prediction_out'),
+           create=True)
     def test_run_single_predictor(self, mock_subprocess, mock_call):
         filename = 'testdata/test.fasta'
         methods = ['psipred', 'predisorder', 'disopred', 'spine']
-        pred = [0,0,0]
+        pred = [0, 0, 0]
 
-        from kman_web.tasks import run_single_predictor        
+        from kman_web.tasks import run_single_predictor
 
-        ## check when there is no result from d2p2
+        # check when there is no result from d2p2
         d2p2_result = [False, []]
         for pred_name in methods:
             expected = [pred_name, pred]
             mock_call.return_value = expected[:]
-            
 
-            result = run_single_predictor.delay(d2p2_result, filename, pred_name) 
+            result = run_single_predictor.delay(d2p2_result,
+                                                filename,
+                                                pred_name)
             eq_(result.get(), expected)
 
-        ## check when there is result from d2p2
+        # check when there is result from d2p2
         d2p2_result = [True, ['D2P2', [0, 0, 0]]]
         expected = d2p2_result[1]
 
-        result = run_single_predictor.delay(d2p2_result, filename, pred_name) 
+        result = run_single_predictor.delay(d2p2_result, filename, pred_name)
         eq_(result.get(),  expected)
-            
-        
-    @patch('subprocess.call') 
+
+    @patch('subprocess.call')
     @raises(RuntimeError)
     def test_run_single_predictor_subprc_error(self, mock_subprocess):
         filename = 'testdata/test.fasta'
         d2p2_result = [False, []]
         mock_subprocess.side_effect = subprocess.CalledProcessError(
             "returncode", "cmd", "output")
-        
+
         from kman_web.tasks import run_single_predictor
-        
+
         result = run_single_predictor.delay(d2p2_result, filename, 'psipred')
         result.get()
-    
-        
-        
-    
-  
-    
-
-    
-           
-
-
-
- 
--- tests/unit/test_zapi.py
+++ tests/unit/test_zapi.py
@@ -5,6 +5,7 @@ from nose.tools import eq_, ok_, raises
 
 from kman_web.factory import create_app
 
+
 class TestEndpoints(object):
 
     @classmethod
@@ -26,7 +27,7 @@ class TestEndpoints(object):
         mock_call.assert_called_once_with('testdata')
 
     @patch('kman_web.services.kman.PredictAndAlignStrategy.__call__')
-    def test_create_kman_predict(self, mock_call):
+    def test_create_kman_predict_and_align(self, mock_call):
         mock_call.return_value = 12345
         rv = self.app.post('/api/create/predict_and_align/',
                            data={'data': 'testdata'})
@@ -44,14 +45,54 @@ class TestEndpoints(object):
         rv = self.app.post('/api/create/predict_and_align/')
         eq_(rv.status_code, 400)
 
-    
     @raises(ValueError)
     def test_get_kman_status_unknown_input_type(self):
         rv = self.app.get('/api/status/unknown/12345/')
         eq_(rv.status_code, 400)
-#
-#    @raises(ValueError)
-#    def test_get_kman_result_unknown_output_type(self):
-#        rv = self.app.get('/api/result/unknown/12345/')
-#        eq_(rv.status_code, 400)
-#
+
+    @raises(ValueError)
+    def test_get_kman_result_unknown_output_type(self):
+        rv = self.app.get('/api/result/unknown/12345/')
+        eq_(rv.status_code, 400)
+
+    @patch('kman_web.tasks.postprocess.AsyncResult')
+    def test_get_xssp_status_predict(self, mock_result):
+        mock_result.return_value.failed.return_value = False
+        mock_result.return_value.status = 'SUCCESS'
+        rv = self.app.get('/api/status/predict/12345/')
+        eq_(rv.status_code, 200)
+        response = json.loads(rv.data)
+        ok_('status' in response)
+        eq_(response['status'], 'SUCCESS')
+
+    @patch('kman_web.tasks.postprocess.AsyncResult')
+    def test_get_xssp_status_predict_and_align(self, mock_result):
+        mock_result.return_value.failed.return_value = False
+        mock_result.return_value.status = 'SUCCESS'
+        rv = self.app.get('/api/status/predict_and_align/12345/')
+        eq_(rv.status_code, 200)
+        response = json.loads(rv.data)
+        ok_('status' in response)
+        eq_(response['status'], 'SUCCESS')
+
+    @patch('kman_web.tasks.postprocess.AsyncResult')
+    def test_get_kman_result_predict(self, mock_result):
+        mock_result.return_value.get.return_value = 'content-of-result'
+        rv = self.app.get('/api/result/predict/12345/')
+        eq_(rv.status_code, 200)
+        response = json.loads(rv.data)
+        ok_('result' in response)
+        eq_(response['result'], {'prediction': 'content-of-result'})
+
+    @patch('kman_web.tasks.postprocess.AsyncResult')
+    def test_get_kman_result_predict_and_align(self, mock_result):
+        mock_result.return_value.get.return_value = ['some_part' for i in range(3)]  \
+            + [['raw_al', 'proc_al']]
+        rv = self.app.get('/api/result/predict_and_align/12345/')
+        eq_(rv.status_code, 200)
+        response = json.loads(rv.data)
+        ok_('result' in response)
+        eq_(response['result'],
+            {'prediction': ['some_part', 'some_part', 'some_part'],
+             'alignment': {'raw': 'raw_al', 'processed': 'proc_al'}})
+
