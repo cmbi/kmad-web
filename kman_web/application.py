d9b7ffd588bdd325110ec6beadcd850b7f553912
Joanna Lange <joasialange@gmail.com>
2014-10-30 10:03:13 +0100

    Improved kman_web.servcies.consensus (python-mode)

--- kman_web/application.py
+++ kman_web/application.py
@@ -1,5 +1,5 @@
 from kman_web.factory import create_celery_app, create_app
 
 
-app = create_app()	
+app = create_app()
 celery = create_celery_app(app)
--- kman_web/services/consensus.py
+++ kman_web/services/consensus.py
@@ -26,25 +26,32 @@ def find_next_length(seq, pos):
 
 def filter_out_short_stretches(cons):
     new_cons = ["filtered", []]
-    prev_length, curr_length, next_length = 0, 0, 0
-    curr_state = cons[0] # current state {no, maybe, yes} {0,1,2}
+    prev_length, curr_length = 0, 0
+    # current state {no, maybe, yes} {0,1,2}
+    prev_state, curr_state = cons[0], cons[0]
     for i in range(len(cons)):
         if cons[i] == curr_state:
             curr_length += 1
-        else:       #the end of the current state - check if it's long enough if not change it to another state & add the elements form the curr_state to the new_cons
+        else:
+            # the end of the current state - check if it's long enough
+            # if not change it to another state & add the elements form
+            # the curr_state to the new_cons
             next_state = cons[i]
-            if curr_length < 4 and curr_length < prev_length and prev_state == next_state and curr_length < find_next_length(cons, i):
-                new_cons[1] += [prev_state for j in range(curr_length)]    # add curr_length elements of previous(=next) state
+            if (curr_length < 4
+                    and curr_length < prev_length
+                    and prev_state == next_state
+                    and curr_length < find_next_length(cons, i)):
+                # add curr_length elements of previous(=next) state
+                new_cons[1] += [prev_state for j in range(curr_length)]
             else:
-                new_cons[1] += [curr_state for j in range(curr_length)]    
+                new_cons[1] += [curr_state for j in range(curr_length)]
             prev_length = curr_length
             prev_state = curr_state
             curr_state = cons[i]
             curr_length = 1
     if curr_length < 4 and curr_length < prev_length:
-        new_cons[1] += [prev_state for j in range(curr_length)]    
+        new_cons[1] += [prev_state for j in range(curr_length)]
     else:
-        new_cons[1] += [curr_state for j in range(curr_length)]     
-    
-    return new_cons 
+        new_cons[1] += [curr_state for j in range(curr_length)]
 
+    return new_cons
