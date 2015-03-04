5e12e7453e56fa245a5a8f310784b3fd0b2e4211
Joanna Lange <joasialange@gmail.com>
2015-03-04 09:31:17 +0100

    Resolving conflicts, part 2

--- kman_web/default_settings.py
+++ kman_web/default_settings.py
@@ -12,7 +12,6 @@ CELERY_QUEUES = (
 CELERY_RESULT_BACKEND = 'redis://localhost/2'
 
 #CELERY_TASK_SERIALIZER = 'json'
-<<<<<<< HEAD
 CELERYBEAT_SCHEDULE = {
     # Every day at midnight
     'update_elmdb': {
@@ -21,25 +20,3 @@ CELERYBEAT_SCHEDULE = {
         'args': ('kman_web/frontend/static/dbs/elm_complete.txt',)
     },
 }
-
-
-## KMAN
-#SPINE_DIR = "/mnt/datas/joanna/disorderPredictionMethods/spine-d/SPINE-D-local/"
-SPINE_DIR = "/home/joanna/software/SPINE-D-local/"
-SPINE_OUTPUT_DIR = SPINE_DIR+"predout/"
-#DISOPRED_PATH ="/mnt/datas/joanna/disorderPredictionMethods/disopred/rundisopred"
-DISOPRED_PATH ="/home/joanna/software/DISOPRED/run_disopred.pl"
-#PREDISORDER_PATH = "/mnt/datas/joanna/disorderPredictionMethods/predisorder/predisorder1.1/bin/predict_diso.sh"
-PREDISORDER_PATH = "/home/joanna/software/predisorder1.1/bin/predict_diso.sh"
-#PSIPRED_PATH = "/mnt/datas/joanna/disorderPredictionMethods/psipred/runpsipred"
-PSIPRED_PATH = "/home/joanna/software/psipred/runpsipred"
-SWISSPROT_DB = "/home/joanna/data/uniprot_sprot.fasta"
-ELM_DB = "/home/joanna/skrypty/elm_classes.tsv"
-PFAM_SCAN = "/home/joanna/bin/PfamScan/pfam_scan.pl"
-PFAM_DB = "/home/joanna/data/Pfam"
-
-UNIPROT_FASTA_DIR = "/home/joanna/data/uniprot_fasta/"
-UNIPROT_DAT_DIR = "/home/joanna/data/uniprot_dat/"
-extension = {"disopred":".diso", "psipred":".ss2", "predisorder": ".predisorder", "spine":".spd"}
-=======
->>>>>>> 3055ed785fc1c05c5b6367fc894cb29091436f84
