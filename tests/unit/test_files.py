69b6b33c7e4eb611db04d978f32ac50a6e47091a
Joanna Lange <joasialange@gmail.com>
2015-03-23 11:13:39 +0100

    Fixed test_files

--- tests/unit/test_files.py
+++ tests/unit/test_files.py
@@ -14,7 +14,8 @@ def test_get_fasta_from_blast(mock_open, test_get_seq):
     # check: query sequence the same as the first blast hit
     reads = ['>testseq1\nSEQ\n', test_blast]
     m_file.read.side_effect = lambda: reads.pop(0)
-    seq_calls = [['>testseq1\n', 'SEQ'], ['>testseq2\n', 'SE']]
+    seq_calls = [['>testseq1\n', 'SEQ'], ['>testseq1\n', 'SEQ'],
+                 ['>testseq2\n', 'SE']]
     test_get_seq.side_effect = lambda x: seq_calls.pop(0)
     expected_data = '>testseq1\nSEQ\n>testseq2\nSE\n'
 
@@ -27,7 +28,8 @@ def test_get_fasta_from_blast(mock_open, test_get_seq):
 
     # check: query sequence different than teh first blast hit
     reads = ['>testseq0\nSEQSEQ\n', test_blast]
-    seq_calls = [['>testseq1\n', 'SEQ'], ['>testseq2\n', 'SE']]
+    seq_calls = [['>testseq1\n', 'SEQ'],['>testseq1\n', 'SEQ'],
+                 ['>testseq2\n', 'SE']]
     expected_data = '>testseq0\nSEQSEQ\n>testseq1\nSEQ\n>testseq2\nSE\n'
     handle.reset_mock()
 
@@ -35,28 +37,28 @@ def test_get_fasta_from_blast(mock_open, test_get_seq):
     handle.write.assert_called_once_with(expected_data)
 
 
-# def test_is_empty():
-#     from kman_web.services.files import is_empty
-#
-#     ok_(is_empty(' '))
-#     ok_(not is_empty('abc'))
-#
-#
-# def test_get_seq_from_uniprot():
-#     from kman_web.services.files import get_seq_from_uniprot
-#
-#     expected = test_vars.ins_human_seq
-#     result = get_seq_from_uniprot('INS_HUMAN')
-#     eq_(expected, result)
-#
-#
-# def test_write_single_fasta():
-#     from kman_web.services.files import write_single_fasta
-#
-#     expected = '>1\nSEQ\nSEQ'
-#     fasta_input = '>1\nSEQ\nSEQ\n>2\nSEQSEQ\n>3\nSEQ\n'
-#     outname = write_single_fasta(fasta_input)
-#     with open(outname) as a:
-#         outfile = a.read()
-#     eq_(expected, outfile)
-#     os.remove(outname)
+def test_is_empty():
+    from kman_web.services.files import is_empty
+
+    ok_(is_empty(' '))
+    ok_(not is_empty('abc'))
+
+
+def test_get_seq_from_uniprot():
+    from kman_web.services.files import get_seq_from_uniprot
+
+    expected = test_vars.ins_human_seq
+    result = get_seq_from_uniprot('INS_HUMAN')
+    eq_(expected, result)
+
+
+def test_write_single_fasta():
+    from kman_web.services.files import write_single_fasta
+
+    expected = '>1\nSEQ\nSEQ'
+    fasta_input = '>1\nSEQ\nSEQ\n>2\nSEQSEQ\n>3\nSEQ\n'
+    outname = write_single_fasta(fasta_input)
+    with open(outname) as a:
+        outfile = a.read()
+    eq_(expected, outfile)
+    os.remove(outname)
