df848cf032e03965c29478cd8f12199fa967902e
Joanna Lange <joasialange@gmail.com>
2015-04-22 11:28:45 +0200

    Fix the list widget (for prediction method form)

--- kmad_web/frontend/dashboard/forms.py
+++ kmad_web/frontend/dashboard/forms.py
@@ -26,7 +26,7 @@ class MyListWidget(object):
     """
     def __init__(self, html_tag='ul', prefix_label=True):
         assert html_tag in ('ol', 'ul', 'collist')
-        self.html_tag = 'ul id="listwidget"'
+        self.html_tag = 'ul'
         self.html_end_tag = self.html_tag.split()[0]
         self.prefix_label = prefix_label
 
