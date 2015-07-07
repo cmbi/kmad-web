ae6b857934bc18c875a2cc918e3bd1129a9df3ea
Joanna Lange <joasialange@gmail.com>
2015-07-07 09:46:56 +0200

    redirect mafft's stderr to subprocess.PIPE

--- kmad_web/services/msa_tools.py
+++ kmad_web/services/msa_tools.py
@@ -22,7 +22,7 @@ def run_preliminary_alignment(fastafile, method):
     if method != 'mafft':
         subprocess.call(args)
     else:
-        output = subprocess.check_output(args, stderr=fastafile+"_log")
+        output = subprocess.check_output(args, stderr=subprocess.PIPE)
         out = open(fastafile + '_prel_al', 'w')
         out.write(''.join(output))
         out.close()
