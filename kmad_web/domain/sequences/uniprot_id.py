29acaa1c16844d77b15b2bd85294f4eae5540b34
Joanna Lange <joasialange@gmail.com>
2015-11-23 14:04:40 +0100

    bugfix in uniprot_id provider

--- kmad_web/domain/sequences/uniprot_id.py
+++ kmad_web/domain/sequences/uniprot_id.py
@@ -9,7 +9,7 @@ _log = logging.getLogger(__name__)
 def get_uniprot_id(sequence):
     fasta_sequence = ">sequence\n{}\n".format(sequence)
     blast = BlastResultProvider(BLAST_DB)
-    blast_result = blast.get_result(fasta_sequence)
+    blast_result = blast.get_result(fasta_sequence, seq_limit=1)
     seq_id = blast.get_exact_hit(blast_result)
     _log.info("seq id: {}".format(seq_id))
     return seq_id
