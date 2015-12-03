6c3dd73686095953ee2bd7fbaa4c3a5b5b43602b
Joanna Lange <joasialange@gmail.com>
2015-12-03 15:00:22 +0100

    celery: use only json celery (gets rid of the stupid celery warning)

--- kmad_web/default_settings.py
+++ kmad_web/default_settings.py
@@ -11,6 +11,10 @@ CELERY_QUEUES = (
 )
 CELERY_RESULT_BACKEND = 'redis://localhost/1'
 
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
 CELERYBEAT_SCHEDULE = {
     # Every month on the 1st at midnight
     'update_elmdb': {
