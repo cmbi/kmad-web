5e97fe0b2f581b8e2f7175cdbaace58d4092fe5c
Joanna Lange <joasialange@gmail.com>
2016-07-05 17:32:05 +0200

    Add a with_setup decorator to the test_get_ptms unit test

--- tests/unit/test_uniprot_feature_provider.py
+++ tests/unit/test_uniprot_feature_provider.py
@@ -1,7 +1,20 @@
-from nose.tools import eq_, ok_
+from nose.tools import eq_, ok_, with_setup
+
+from kmad_web.services.helpers.cache import cache_manager as cm
 from kmad_web.domain.features.providers.uniprot import UniprotFeatureProvider
 
 
+def setup():
+    cm.load_config({
+        'redis': {'redis.backend': 'dogpile.cache.null'}
+    })
+
+
+def teardown():
+    cm.reset()
+
+
+@with_setup(setup, teardown)
 def test_get_ptms():
     uniprot = UniprotFeatureProvider()
     p = uniprot.get_ptms('TAU_HUMAN')
