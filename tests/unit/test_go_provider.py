ca913cfcd1b22acc17aa8fad611f7bf8c86d6948
Joanna Lange <joasialange@gmail.com>
2017-02-03 14:07:23 +0100

    Update test_go_provider; #28

--- tests/unit/test_go_provider.py
+++ tests/unit/test_go_provider.py
@@ -1,4 +1,6 @@
 from nose.tools import eq_, with_setup
+from mock import Mock, patch
+
 from kmad_web.domain.go.providers.go import GoProvider
 from kmad_web.services.helpers.cache import cache_manager as cm
 
@@ -13,10 +15,14 @@ def teardown():
     cm.reset()
 
 
+@patch('kmad_web.services.go.GoService.call')
 @with_setup(setup, teardown)
-def test_get_children_terms():
+def test_get_children_terms(mock_call):
 
     go_term = 'GO:0070137'
+    mock_call.return_value = {
+        'item': [{'key': 'GO:0070139'}]
+    }
 
     expected = set(['GO:0070139'])
 
@@ -24,15 +30,19 @@ def test_get_children_terms():
     go.get_children_terms(go_term)
     eq_(expected, go.children)
 
-    go_term = '0070137'
-    go.get_children_terms(go_term)
-    eq_(expected, go.children)
+    # go_term = '0070137'
+    # go.get_children_terms(go_term)
+    # eq_(expected, go.children)
 
 
+@patch('kmad_web.services.go.GoService.call')
 @with_setup(setup, teardown)
-def test_get_parent_terms():
+def test_get_parent_terms(mock_call):
 
     go_term = 'GO:0070139'
+    mock_call.return_value = {
+        'item': [{'key': 'GO:0070137'}, {'key': 'GO:0016929'}]
+    }
 
     expected = set(['GO:0070137', 'GO:0016929'])
 
