5f77bdb8ed6e226fd79a179a5a483045213aa36b
Coos Baakman <cbaakman@radboudumc.nl>
2017-02-07 12:24:01 +0100

    make sure that gunzip always works, not just the first time

--- update_databanks.bash
+++ update_databanks.bash
@@ -9,5 +9,5 @@ done
 if ! [ -d blast ] ; then mkdir blast; fi
 cd blast
 /usr/bin/wget -N ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
-/bin/gunzip uniprot_sprot.fasta.gz
+/bin/gunzip -f uniprot_sprot.fasta.gz
 /usr/bin/makeblastdb -in uniprot_sprot.fasta -out sprot -dbtype prot -parse_seqids
