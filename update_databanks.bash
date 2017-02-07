bcf7580fc920de6725434032604a91ea601b806a
Coos Baakman <cbaakman@radboudumc.nl>
2017-02-07 12:20:25 +0100

    make sure that tar always works during an update, not just the first time

--- update_databanks.bash
+++ update_databanks.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd /data
-/usr/bin/wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/nr.\*.tar.gz
+/usr/bin/wget -N ftp://ftp.ncbi.nlm.nih.gov/blast/db/nr.\*.tar.gz
 for ar in nr.*.tar.gz; do
-    /bin/tar xzf $ar
+    /bin/tar --overwrite -xzf $ar
 done
 
 if ! [ -d blast ] ; then mkdir blast; fi
 cd blast
-/usr/bin/wget ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
+/usr/bin/wget -N ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 /bin/gunzip uniprot_sprot.fasta.gz
 /usr/bin/makeblastdb -in uniprot_sprot.fasta -out sprot -dbtype prot -parse_seqids
