0e6fe9f2a9afdd91dcd12a41e714704ad5a8992b
Coos Baakman <cbaakman@chelonium.cmbi.umcn.nl>
2017-05-22 13:52:40 +0200

    updat databanks container: make it generate uniref90 database

--- update_databanks.bash
+++ update_databanks.bash
@@ -11,3 +11,14 @@ cd blast
 /usr/bin/wget -N ftp://ftp.ebi.ac.uk/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz
 /bin/gunzip -f uniprot_sprot.fasta.gz
 /usr/bin/makeblastdb -in uniprot_sprot.fasta -out sprot -dbtype prot -parse_seqids
+
+cd /data
+if ! [ -d uniref ] ; then mkdir uniref; fi
+cd uniref
+/usr/bin/wget -N ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz
+/bin/gunzip -f uniref90.fasta.gz
+/usr/local/bin/pfilt uniref90.fasta > uniref90filt.fasta
+/usr/bin/formatdb -i uniref90filt.fasta -t uniref90filt
+mv uniref90filt.fasta.pal uniref90filt.pal
+/usr/bin/formatdb -i uniref90.fasta -t uniref90
+mv uniref90.fasta.pal uniref90.pal
