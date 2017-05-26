353e089bff4db8c9e260bcf4ab05b9a22589bbd0
Coos Baakman <cbaakman@chelonium.cmbi.umcn.nl>
2017-05-26 14:52:24 +0200

    fix for formatdb, removed unnecessary files also

--- update_databanks.bash
+++ update_databanks.bash
@@ -3,7 +3,10 @@
 cd /data
 /usr/bin/wget -N ftp://ftp.ncbi.nlm.nih.gov/blast/db/nr.\*.tar.gz
 for ar in nr.*.tar.gz; do
-    /bin/tar --overwrite -xzf $ar
+    name=${ar%.tar.gz}
+    if [ $ar -nt $name'.psq' ] ; then
+        /bin/tar --overwrite -xzf $ar
+    fi
 done
 
 if ! [ -d blast ] ; then mkdir blast; fi
@@ -16,12 +19,14 @@ cd /data
 if ! [ -d uniref ] ; then mkdir uniref; fi
 cd uniref
 /usr/bin/wget -N ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz
-/bin/gunzip -f uniref90.fasta.gz
-/usr/local/bin/pfilt uniref90.fasta > uniref90filt.fasta
-/usr/bin/formatdb -i uniref90filt.fasta -t uniref90filt
-mv uniref90filt.fasta.pal uniref90filt.pal
-/usr/bin/formatdb -i uniref90.fasta -t uniref90
-mv uniref90.fasta.pal uniref90.pal
+if [ uniref90.fasta.gz -nt uniref90.pal ] ; then
+    /bin/gunzip -f uniref90.fasta.gz
+    /usr/local/bin/pfilt uniref90.fasta > uniref90filt.fasta
+    /usr/local/bin/formatdb -i uniref90filt.fasta -t uniref90filt
+    mv uniref90filt.fasta.pal uniref90filt.pal
+    /usr/local/bin/formatdb -i uniref90.fasta -t uniref90
+    mv uniref90.fasta.pal uniref90.pal
+fi
 
 cd /
 /usr/bin/wget -N http://download.igb.uci.edu/sspro4.tar.gz
