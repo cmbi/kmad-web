be1add4f07afa8ee26bcfb85341107b510be713d
joannalange <joasialange@gmail.com>
2020-02-18 17:13:33 +0100

    Add a test case for uniprot feature provider

--- tests/unit/test_uniprot_feature_provider.py
+++ tests/unit/test_uniprot_feature_provider.py
@@ -17,8 +17,137 @@ def teardown():
 @with_setup(setup, teardown)
 def test_get_ptms():
     uniprot = UniprotFeatureProvider()
-    p = uniprot.get_ptms('TAU_HUMAN')
-    ok_(p)
+    result = uniprot.get_ptms('TAU_HUMAN')
+    ok_(result)
+
+    result = uniprot.get_ptms('P21815')
+    expected = [{
+        "info": "Phosphoserine",
+        "position": 31,
+        "annotation_level": 0,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Phosphoserine",
+        "position": 67,
+        "annotation_level": 1,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Phosphoserine",
+        "position": 74,
+        "annotation_level": 1,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Phosphoserine",
+        "position": 75,
+        "annotation_level": 1,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Phosphoserine",
+        "position": 94,
+        "annotation_level": 1,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Phosphoserine",
+        "position": 100,
+        "annotation_level": 1,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Phosphoserine",
+        "position": 149,
+        "annotation_level": 1,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Phosphoserine",
+        "position": 280,
+        "annotation_level": 1,
+        "name": "phosphorylation"
+    },
+    {
+        "info": "Sulfotyrosine",
+        "position": 313,
+        "annotation_level": 0,
+        "name": "Sulfotyrosine"
+    },
+    {
+        "info": "Sulfotyrosine",
+        "position": 314,
+        "annotation_level": 0,
+        "name": "Sulfotyrosine"
+    },
+    {
+        "info": "N-linked (GlcNAc...) asparagine",
+        "position": 104,
+        "annotation_level": 1,
+        "name": "N-glycosylation"
+    },
+    {
+        "info": "O-linked (GalNAc...) threonine",
+        "position": 119,
+        "annotation_level": 2,
+        "name": "O-glycosylation"
+    },
+    {
+        "info": "O-linked (GalNAc...) threonine",
+        "position": 122,
+        "annotation_level": 2,
+        "name": "O-glycosylation"
+    },
+    {
+        "info": "N-linked (GlcNAc...) asparagine",
+        "position": 177,
+        "annotation_level": 1,
+        "name": "N-glycosylation"
+    },
+    {
+        "info": "N-linked (GlcNAc...) asparagine",
+        "position": 182,
+        "annotation_level": 1,
+        "name": "N-glycosylation"
+    },
+    {
+        "info": "N-linked (GlcNAc...) asparagine",
+        "position": 190,
+        "annotation_level": 1,
+        "name": "N-glycosylation"
+    },
+    {
+        "info": "O-linked (GalNAc...) threonine",
+        "position": 227,
+        "annotation_level": 2,
+        "name": "O-glycosylation"
+    },
+    {
+        "info": "O-linked (GalNAc...) threonine",
+        "position": 228,
+        "annotation_level": 2,
+        "name": "O-glycosylation"
+    },
+    {
+        "info": "O-linked (GalNAc...) threonine",
+        "position": 229,
+        "annotation_level": 2,
+        "name": "O-glycosylation"
+    },
+    {
+        "info": "O-linked (GalNAc...) threonine",
+        "position": 238,
+        "annotation_level": 2,
+        "name": "O-glycosylation"
+    },
+    {
+        "info": "O-linked (GalNAc...) threonine",
+        "position": 239,
+        "annotation_level": 2,
+        "name": "O-glycosylation"
+    }]
+    eq_(result, expected)
 
 
 def test_get_ptm_type():
